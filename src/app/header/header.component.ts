5cd4b56be2dd550cc55d0195a94e6745768488f4
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-02-02 18:18:43 -0200

    Change header layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,13 +10,13 @@ import { faBlog } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent implements OnInit {
 
   faBlog = faBlog;
-
+  private authority: string = null;
   lastActiveElement: any = null;
 
   constructor() { }
 
   ngOnInit() {
-
+    
   }
 
   setActive(event: any){
@@ -28,5 +28,51 @@ export class HeaderComponent implements OnInit {
   logout(){
     console.log("logout")
   }
+
+  getLocationPath(){
+    return "/";
+  }
+
+  // private authority: string;
+
+  // constructor(private tokenStorageService: TokenStorageService, private router: Router, 
+  //     location: Location, private toasterService: ToasterService) {};
+
+  // getLOcationPath(){
+  //     return location.pathname;
+  // }
+
+  // ngOnInit() {
+  //     this.updateNavigation();
+  //     this.tokenStorageService.getTheBoolean().subscribe(
+  //         value => { 
+  //             this.updateNavigation() 
+  //         }
+  //     );
+  // }
+
+  // updateNavigation(){
+  //     this.authority = null;
+  //     // get authority from using auth service
+  //     console.log("Updating navigaation component")
+  //     let roles = this.tokenStorageService.getAuthorities();
+  //     console.log(roles);
+  //     roles.every(role => {
+  //         this.authority = role;
+  //         console.log(this.authority)
+  //         if(role === 'ROLE_ADMIN'){
+  //             return false
+  //         }
+  //         return true; // continue iterating the role vector
+  //     });
+  // }
+
+  // logout() {
+  //     this.tokenStorageService.signOut();
+  //     //window.location.reload();
+  //     this.toasterService.success("You have been logged out successfully.", true);
+  //     this.router.navigate(['']);
+  // }
+
 
 }
