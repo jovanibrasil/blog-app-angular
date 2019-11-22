cb5d769355845b86c1c5a16d49a6ed6c4f27b726
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-11-22 04:55:08 -0300

    Add basic pagination buttons

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,26 +13,49 @@ export class MainComponent implements OnInit {
   summaries: Summary[] = [];
   loading: boolean = true;
 
+  // Pagination parameters
+  p: number = 1;
+  count: number = 1; // number of pages
+
+  i: number = 0; // pagination index
+
   constructor(private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      //this.getFullPostById(+params.get('id'));
       let tag = params.get('tag');
       tag = tag ? tag : "all" ;
-      this.getLastPostsSummaries(tag);
+      this.getLastPostsSummaries(tag, 0);
     })
   }
 
-  getLastPostsSummaries(tag: string){
+  getLastPostsSummaries(tag: string, page: number){
     this.loading = true;
-    this.postService.getLastPostsSummaries(0, tag).subscribe(
+    this.postService.getLastPostsSummaries(page, tag).subscribe(
       res => { 
-        this.summaries = res.data;
+        this.summaries = res.data.summaries;
         this.loading = false;
+        this.count = res.data.pages;
       }, 
       err => {}
     );
+    this.p = page;
+  }
+
+
+  nextPage(){
+    console.log("i="+this.i);
+    if(this.p+1 <= this.count){
+      this.p++;
+      this.getLastPostsSummaries("all", this.p);
+    }
+  }
+
+  previousPage(){
+    if(this.p > 0){
+      this.p--;
+      this.getLastPostsSummaries("all", this.p);
+    }
   }
 
 }
