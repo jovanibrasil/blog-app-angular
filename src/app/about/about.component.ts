ae2b605a1619a783416c5cc57b95494eed9a68a8
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-05-02 02:52:52 -0300

    Add personal information on the left side bar

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  user: User;
+
   constructor() { }
 
   ngOnInit() {
+    this.user = { 
+      email: "jovanibrasil.com", 
+      gitHubUserName: "jovanibrasil", 
+      fullName: "Jovani Brasil", 
+      googleScholarUrl: "https://scholar.google.com.br/citations?user=nQYs1z4AAAAJ&hl=en", 
+      lattesUrl: "http://lattes.cnpq.br/3411257639128251", 
+      linkedInUserName: "jovanibrasil",  phone: null, userName: "jovanibrasil" 
+    } 
   }
 
 }
